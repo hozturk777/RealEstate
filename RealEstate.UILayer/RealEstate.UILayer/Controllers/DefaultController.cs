1e8ad1e55f3c24d42bbf166e1371700e7d1ad482
Hüseyin Öztürk <b_dd0_huseyin_06@outlook.com>
2022-11-09 16:10:19 +0300

    ,

---
+++ RealEstate.UILayer/RealEstate.UILayer/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.UILayer.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult Index5()
+        {
+            return View();
+        }
+    }
+}
